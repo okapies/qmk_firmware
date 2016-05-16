05f506c6b09ee81564d2749bc672c8ebe8e25deb
Yuta Okamoto <okapies@gmail.com>
2016-05-17 02:00:36 +0900

    assign keys to emulate the JIS layout

--- keyboard/ergodox_ez/keymaps/kines-ish-ja/keymap.c
+++ keyboard/ergodox_ez/keymaps/kines-ish-ja/keymap.c
@@ -10,15 +10,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  | MHEN |           | HENK |   6  |   7  |   8  |   9  |   0  |   -=   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   @`   |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;+  |   :*   |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |  ,<  |  .>  |  /?  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Esc  | Grv  |Insert| Left | Right|                                       |  Up  | Down |   [  |   ]  |  L2  |
+ *   |Grv/L1|  ^~  |  \|  | Left | Right|                                       |  Up  | Down |   [{ |  ]}  |  \_  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Ctrl | Alt  |       | LGui | Ctrl |
@@ -32,20 +32,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_LEFT,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  TG(SYMB),
-        KC_LCTL,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
-        KC_ESC,   KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
-                                               KC_LCTL,KC_LALT,
-                                                       KC_HOME,
-                                         KC_BSPC,KC_DEL,KC_END,
+        KC_ESC,          KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_MHEN,
+        KC_TAB,          KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  TG(SYMB),
+        KC_LCTL,         KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
+        KC_LSFT,         KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  ALL_T(KC_NO),
+        LT(SYMB,KC_GRV), KC_EQL,  KC_JYEN, KC_LEFT,  KC_RGHT,
+                                                      KC_LCTL,KC_LALT,
+                                                              KC_HOME,
+                                                KC_BSPC,KC_DEL,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_BSLS,
+             KC_HENK,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
+             TG(SYMB),    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
              MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,  KC_FN2,
+                                  KC_UP,  KC_DOWN,KC_RBRC,KC_BSLS,  KC_RO,
              KC_LGUI, KC_RCTL,
              KC_PGUP,
              KC_PGDN, KC_ENT, KC_SPC
